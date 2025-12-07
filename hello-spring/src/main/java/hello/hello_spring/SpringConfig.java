ac71b51812e2ba651a6afab01d0836a68e85ace5
nmhyrhn <gbs06193@gmail.com>
2025-12-07 23:28:27 +0900

    251207

--- hello-spring/src/main/java/hello/hello_spring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hello_spring/SpringConfig.java
@@ -1,10 +1,14 @@
 package hello.hello_spring;
 
+import hello.hello_spring.aop.TimeTraceAop;
 import hello.hello_spring.repository.JdbcMemberRepository;
 //import hello.hello_spring.repository.JdbcTemplateMemberRepository;
+import hello.hello_spring.repository.JpaMemberRepository;
 import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemoryMemberRepository;
 import hello.hello_spring.service.MemberService;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,21 +17,36 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-    private final DataSource dataSource;
+//   SpringDataJpa
+    private final MemberRepository  memberRepository;
 
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
+    //    private EntityManager em;
+//
+//    @Autowired
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
-// return new MemoryMemberRepository();
-//        return new JdbcMemberRepository(dataSource);
-        return new JdbcMemberRepository(dataSource);
-    }
+//    @Bean
+//    public TimeTraceAop timeTraceAop() {
+//        return new TimeTraceAop();
+//    }
+
+//    @Bean
+//    public MemberRepository memberRepository() {
+//// return new MemoryMemberRepository();
+////        return new JdbcMemberRepository(dataSource);
+////        return new JdbcMemberRepository(dataSource);
+////        return new JpaMemberRepository(em);
+//
+//    }
 }
--- hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
@@ -5,12 +5,13 @@ import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 //@Service
-
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -22,10 +23,19 @@ public class MemberService {
 
 //    회원 가입
     public Long join(Member member) {
+
+        long start = System.currentTimeMillis();
+
 //        같은 이름이 있는 중복 회원 x
-        validateDuplicateMember(member);//중복 회원 검증
-        memberRepository.save(member);
-        return member.getId();
+        try {
+            validateDuplicateMember(member);//중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = " + timeMs + "ms");
+        }
     }
 
     private void validateDuplicateMember(Member member) {
@@ -37,8 +47,16 @@ public class MemberService {
 
     //   전체 회원 조회
     public List<Member> findMembers(){
-        return memberRepository.findAll();
+        long start = System.currentTimeMillis();
+        try {
+            return memberRepository.findAll();
+        } finally {
+        long finish = System.currentTimeMillis();
+        long timeMs = finish - start;
+        System.out.println("findMembers = " + timeMs + "ms");
     }
+        }
+
 
     public Optional<Member> findOne(Long memberId){
         return memberRepository.findById(memberId);
